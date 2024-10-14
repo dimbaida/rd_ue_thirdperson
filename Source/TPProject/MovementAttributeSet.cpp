fb20db920fc2686caa20b620e27631c40119276f
Dmytro Baidachnyi <dimbaida@gmail.com>
2024-10-15 00:37:16 +0300

    lesson_24 homework

--- Source/TPProject/MovementAttributeSet.cpp
+++ Source/TPProject/MovementAttributeSet.cpp
@@ -3,3 +3,28 @@
 
 #include "MovementAttributeSet.h"
 
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+void UMovementAttributeSet::PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue)
+{
+	Super::PostAttributeChange(Attribute, OldValue, NewValue);
+	if (Attribute == GetSpeedAttribute())
+	{
+		AActor* Player = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);	
+		if (Player)
+		{
+			ACharacter* Character = Cast<ACharacter>(Player);
+			if (Character)
+			{
+				UCharacterMovementComponent* MovementComponent = Character->GetCharacterMovement();
+				if (MovementComponent)
+				{
+					MovementComponent->MaxWalkSpeed = NewValue;
+				}
+			}
+		}
+	}
+
+}
--- Source/TPProject/MovementAttributeSet.h
+++ Source/TPProject/MovementAttributeSet.h
@@ -11,6 +11,8 @@ class TPPROJECT_API UMovementAttributeSet : public UTPAttributeSet
 	GENERATED_BODY()
 
 public:
+	virtual void PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue) override;
+	
 	UPROPERTY(BlueprintReadOnly)
 	FGameplayAttributeData Speed;
 	ATTRIBUTE_ACCESSORS(UMovementAttributeSet, Speed);
